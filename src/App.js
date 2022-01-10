5b0b27fcf8bc4b40675a83c361d49a938a3416c2
unknown <sajeel.ahmed@protonmail.com>
2022-01-11 02:18:10 +0500

    :new: Add Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import ProductDetail from './MicroFrontends/Product/ProductDetail'
+import Landing from './MicroFrontends/Landing/Landing'
+import Products from './MicroFrontends/Products/Products'
 
 function App() {
   return (
     <div className='App'>
-      <ProductDetail />
+      <Routes>
+        <Route path='/antiques' element={<Products />} />
+      </Routes>
+
+      <Routes>
+        <Route path='/product-detail' element={<ProductDetail />} />
+      </Routes>
+
+      <Routes>
+        <Route exact path='/' element={<Landing />} />
+      </Routes>
     </div>
   )
 }
