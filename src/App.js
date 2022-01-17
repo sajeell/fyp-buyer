566b86a97c8202282dfee283852403dd478c2e98
unknown <sajeel.ahmed@protonmail.com>
2022-01-17 10:08:00 +0500

    :new: Setup Protected Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css'
 import { useSelector } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import ProductDetail from './MicroFrontends/Product/ProductDetail'
 import Landing from './MicroFrontends/Landing/Landing'
@@ -9,22 +10,51 @@ import Products from './MicroFrontends/Products/Products'
 import Login from './MicroFrontends/Login/Login'
 
 function App() {
+  const [authenticated, setAuthenticated] = useState(false)
+  const token = useSelector((state) => state.user.token)
+
+  useEffect(() => {
+    if (token) {
+      if (token.length > 0) {
+        setAuthenticated(true)
+      }
+    }
+  }, [token])
+
   return (
     <div className='App'>
       <Routes>
-        <Route path='/antiques' element={<Products />} />
+        <Route
+          path='/antiques'
+          element={authenticated === true ? <Products /> : <Navigate to='/' />}
+        />
       </Routes>
 
       <Routes>
-        <Route path='/product-detail' element={<ProductDetail />} />
+        <Route
+          path='/product-detail'
+          element={
+            authenticated === true ? <ProductDetail /> : <Navigate to='/' />
+          }
+        />
       </Routes>
 
       <Routes>
-        <Route exact path='/landing' element={<Landing />} />
+        <Route
+          exact
+          path='/landing'
+          element={authenticated === true ? <Landing /> : <Navigate to='/' />}
+        />
       </Routes>
 
       <Routes>
-        <Route exact path='/' element={<Login />} />
+        <Route
+          exact
+          path='/'
+          element={
+            authenticated === false ? <Login /> : <Navigate to='/landing' />
+          }
+        />
       </Routes>
 
       <ToastContainer />
