f40fb90bd1683508123f5323df4f331228b0fc16
unknown <sajeel.ahmed@protonmail.com>
2022-03-24 14:20:53 +0500

    :new: Implement Cart Frontend

--- src/MicroFrontends/Cart/Cart.jsx
+++ src/MicroFrontends/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap'
+import { Container, Button, Table } from 'react-bootstrap'
 import Footer from '../Footer/Footer'
 import InnerHeader from '../InnerHeader/InnerHeader'
 import './Cart.css'
@@ -19,27 +19,131 @@ const Cart = () => {
         </div>
       </Container>
       <hr />
-      <Container>
+      <Container style={{ minHeight: '100vh' }}>
         <div className='cart-container'>
           <p className='page-subtitle'>BARGAIN</p>
-          <table>
-            <tr>
-              <th>ITEM</th>
-              <th>REQUESTED ON</th>
-              <th>REQUESTS</th>
-              <th>ACTIONS</th>
-            </tr>
-            <tr>
-              <td className='table-img-container'>
-                <img src={guitarImage} alt='Guitar' />
-                <span>Acoustic Guitar</span>
-              </td>
-              <td>2021-10-12</td>
-              <td>12</td>
-              <td>View Details</td>
-            </tr>
-          </table>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>ITEM</th>
+                <th>REQUESTED ON</th>
+                <th>REQUESTS</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          <br />
           <p className='page-subtitle'>BID</p>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>ITEM</th>
+                <th>REQUESTED ON</th>
+                <th>REQUESTS</th>
+                <th>ACTIONS</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+              <tr>
+                <td className='table-img-container'>
+                  <img src={guitarImage} alt='Guitar' />
+                  <span>Acoustic Guitar</span>
+                </td>
+                <td>2021-10-12</td>
+                <td>12</td>
+                <td>
+                  <Button
+                    variant='outline-info px-4'
+                    style={{ borderRadius: 50 }}
+                  >
+                    View Details
+                  </Button>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
         </div>
       </Container>
       <Footer />
